2b6fa33fc5086e6e38641082697ee6364bb73497
Mocaryova <misa965@gmail.com>
2020-10-03 16:22:26 +0200

    Update index.js

--- saturday/index.js
+++ saturday/index.js
@@ -1,1 +1,25 @@
-console.log("miska");
+let count = 10;
+undefined
+for (i=0; i<count;i++) {
+    if (isEven(i)) {
+    console.log(i);}
+    function isEven(number) {
+        var even = number % 2==0;
+        return even;
+      }
+    }
+    
+let names = ["Mark","CLoe","Wonho"];  
+greet(names[1]);
+
+function greet (names) {
+    console.log("hello "+names);
+}
+let printValues = (array) => {
+    for (let i = 0; i < array.length; i++) {
+      console.log(array[i]);
+    }
+  };
+  printValues ([0,5,6,4,7]);
+  names.forEach;
+  console.log(array[i]);
