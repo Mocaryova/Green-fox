a4c90ecb3ee0830058c1ce771b340b4d58cf9685
Mocaryova <misa965@gmail.com>
2020-10-03 14:41:16 +0200

    js

---
+++ index.js
@@ -0,0 +1,15 @@
+let names = ["Mark","CLoe","Wonho"];
+console.log(names[2].length);
+names.push("obama");
+console.log(names);
+names[0] = "Miska";
+console.log(names);
+console.log(names.length);
+
+let object = {
+    HasManyColors: true,
+    colors: ["puple","blue","yellow","red"],
+};
+if (object.colors.length>3) {
+HasManyColors = true};
+
