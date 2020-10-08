4a7711df4a2dc749963cc052211549ecc9dcc9ae
Mocaryova <misa965@gmail.com>
2020-10-08 18:19:43 +0200

    gallery

--- exercise/index.js
+++ exercise/index.js
@@ -1,3 +1,3 @@
 $("#one").text("Macrotis");
 $("#two").css("border-color","purple");
-$("p",).css("background-color","yellow");
+$("p").css("background-color","yellow");
---
+++ exercise2/index.js
@@ -0,0 +1,12 @@
+let color="purple";
+let number=10;
+let word="cool";
+if (color==="purple") {
+    $('.red').css("background-color","purple");
+}
+if (number>100) {
+    console.log("whoah, that's a big number");
+}
+    else {
+        console.log("just a regular number, please.");
+    }
